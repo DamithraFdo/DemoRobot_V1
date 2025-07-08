593fc163c6cc091d6f926cccb1ac6cfa1b406d2e
Medhavi Eswarage <97176530+Mesheswarage@users.noreply.github.com>
2025-07-08 09:38:59 +0530

    Add files via upload

---
+++ Separate_codes/3MODES_obstacle_avoiding.ino
@@ -0,0 +1,408 @@
+/*
+ # DemoRobot_V1
+ * This is about a demonstrable DIY robot application project
+ * All rights reserved under the R&D team Anka Robotics by Anka Technologies
+ * Developers: Damithrafdo, Shanuka, Kalaynilavan, Medhavi
+ * Under the supervision of Dr. Tharindu
+ * 
+ * Development Board: Arduino Uno
+ * Functions: Obstacle avoiding, line following...
+ * Sensors: Ultrasonic sensor array, IR line follower array
+ * Motors: 2 DC motors with motor driver
+ * 
+ * Project Name: DemoRobot_V1
+ * Version: 1.0 
+ * Date: 2025-06-16
+ * Description:
+ * This code is for a simple robot that can follow a line using an IR sensor array and avoid obstacles using an ultrasonic sensor array.
+ * The robot can move forward, backward, turn left, and turn right based on the readings from the IR sensors.
+ * The robot is controlled using an Arduino Uno board, which receives input from the sensors and sends signals to the motors.
+ * The robot can be programmed to follow a line, avoid obstacles, and perform other tasks based on the sensor readings.
+ * This code is a basic implementation of a line-following robot with obstacle avoidance capabilities.
+ * 
+ * Diagram        
+ *          __________________________
+ *         |                          |
+ *         | Ultrasonic Sensor Array  |
+ *         |__________________________|             _____________
+ *                       |                         |             |
+ *                       |-------------------------| Servo motor |
+ *          __________________________    |        |_____________|
+ *         |  (ir0,ir1,ir2,ir3,ir4)   |   |        
+ *         |  Line Follower IR Array  |   |
+ *         |__________________________|   |
+ *                       |                |
+ *                       |-----------------
+ *                 ______|______
+ *                |             |
+ *                | Arduino Uno |
+ *                |_____________|
+ *                       |
+ *  __________     ______|______     ___________
+ * |   (lm)   |   |             |   |   (rm)    |  
+ * |Left Motor|---| MotorDriver |---|Right Motor| 
+ * |__________|   |_____________|   |___________|
+ *            
+ *            
+ * This code includes           
+ *            
+ */
+
+//Add Libries here - #include<libry.h>
+#include <NewPing.h> 
+#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+
+//Add difines here - #define app A0
+// Ultrasonic sensor pins
+#define TRIG_PIN A1      // Main ultrasonic sensor
+#define ECHO_PIN A2
+#define TRIG_DOWN A0     // Downward ultrasonic sensor
+#define ECHO_DOWN A3
+#define MAX_DISTANCE 200 
+
+// Servo pin
+#define SERVO_PIN 13
+
+// OLED
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Button
+#define BUTTON_PIN 5
+
+//Put variables here - int Val
+
+//const int rme=10,lme=11; //enable pins, Speed
+const int rmb=12, rmf=11, lmb=10, lmf=9; //motor signals, left/right motor back/forward
+const int ir0=2, ir1=3, ir2=4, ir3=5, ir4=6; //ir signals white=1 and black=0
+int val0=0, val1=0, val2=0, val3=0, val4=0; //variables
+int THRESHOLD_DISTANCE = 30;// the distance (in cm) for obstacle detection
+int modeCount = 0; // Variable to track the mode
+bool lastState = HIGH;
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+NewPing downSonar(TRIG_DOWN, ECHO_DOWN, MAX_DISTANCE);
+Servo myservo;
+int distance = 100;
+int downDistance = 100;
+
+//OLED 4 5
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(rmb, OUTPUT);
+  pinMode(rmf, OUTPUT);
+  pinMode(lmb, OUTPUT);
+  pinMode(lmf, OUTPUT);
+//  pinMode(rme, OUTPUT);
+//  pinMode(lme, OUTPUT);
+  pinMode(ir0, INPUT);
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
+  pinMode(ir3, INPUT);
+  pinMode(ir4, INPUT);
+  // Ultrasonic Sensor
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIG_DOWN, OUTPUT);
+  pinMode(ECHO_DOWN, INPUT);
+  // Servo setup
+  myservo.attach(SERVO_PIN);
+  myservo.write(90);  // Center
+  delay(1000);
+
+  // OLED setup
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    for (;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.display();
+
+  // pushbutton setup
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // ModeC();
+  bool currentState = digitalRead(BUTTON_PIN);
+
+  if (lastState == LOW && currentState == HIGH) {
+    modeCount = (modeCount + 1) % 3; // Cycle through 0, 1, 2
+    Serial.print("Mode changed to: ");
+    Serial.println(modeCount);
+  }
+
+  lastState = currentState; 
+
+  switch (modeCount) {
+    case 0:
+      ModeA();
+      Serial.println("A");
+      break;
+    case 1:
+      ModeB();
+      Serial.println("B");
+      break;
+    case 2:
+      ModeC();
+      Serial.println("C");
+      break;
+    // case 3:
+    //   ModeD();
+    //   break;
+    default:
+      break;
+  }
+}
+
+//Add functions here - 
+/*func(_){
+ *  if(_){
+ *  }
+ *  else{
+ *  }
+ *  retern;
+ * }
+ */
+// Display function for OLED 
+void showOnOLED(const char* msg) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(msg);
+  display.display();
+}
+// Reads front distance
+int readDistance() {
+  delay(50);
+  int cm = sonar.ping_cm();
+  if (cm == 0) cm = 250;
+  return cm;
+}
+
+// Reads downward distance
+int readDownDistance() {
+  delay(50);
+  int cm = downSonar.ping_cm();
+  if (cm == 0) cm = 250;
+  return cm;
+}
+
+// Analyze path using servo + main ultrasonic
+void analyzeFreePath() {
+  int distanceRight = lookRight();
+  delay(300);
+  int distanceLeft = lookLeft();
+  delay(300);
+
+  if (distanceRight >= distanceLeft) {
+    Turn(true);
+  } else {
+    Turn(false);
+  }
+}
+
+ void ModeA(){
+  //Obstacle avoidance 
+  showOnOLED("Mode A");
+
+  float distance = readDownDistance();
+  if (distance < THRESHOLD_DISTANCE) {
+    stop();
+    delay(500);       // Pause for a moment
+    InALine(false); // Reverse a bit
+    delay(500); 
+    Turn(false); //  Turn away from obstacle
+    delay(700);   
+  } else {
+    InALine(true); 
+  }
+ }
+ 
+ void ModeB(){
+  //Obstacle avoidance with servo
+  showOnOLED("Mode B");
+
+  distance = readDistance();
+  downDistance = readDownDistance();
+  Serial.print("Front: ");
+  Serial.print(distance);
+  Serial.print(" cm, Down: ");
+  Serial.println(downDistance);
+
+  if (downDistance <= THRESHOLD_DISTANCE) {  // Adjust threshold for small obstacles under the robot
+    stop();
+    delay(300);
+    analyzeFreePath();
+    return;  // Skip other checks in this loop
+  }
+  else if (distance <= THRESHOLD_DISTANCE) {
+    stop();
+    delay(300);
+    InALine(false);
+    delay(400);
+    stop();
+    delay(300);
+    analyzeFreePath();
+  } else {
+    InALine(true);
+  }
+ }
+ 
+ void ModeC(){
+  //Line following
+  showOnOLED("Mode C");
+
+  val0=digitalRead(ir0); 
+  val1=digitalRead(ir1);
+  val2=digitalRead(ir2);
+  val3=digitalRead(ir3);
+  val4=digitalRead(ir4);
+  if(val0 && !val1 && val2) //forward (1&0&1)
+  {
+    bool x=true;
+    InALine(x); 
+  }
+  else if(!val0 && !val1 && val2)  //turn right (1&0&0)
+  {
+    bool y=true;
+    Turn(y);
+  }
+  else if(val0 && !val1 && !val2)//turn left (0&1&1)
+  {
+    bool y=false;
+    Turn(y);
+  }
+  else if(val0 && val1 && val2) //stop (1&1&1)
+  {
+    stop();
+  }
+  else if(!val0 && !val1 && !val2) //forward (0&0&0)
+  {
+    bool x=true;
+    InALine(x);
+  }
+  delay(1000);
+ }
+
+void ModeD(){
+ 
+  showOnOLED("Mode D");
+
+  val0=digitalRead(ir0); // Left sensor
+  val4=digitalRead(ir4); // Right sensor
+  
+  if(!val0 && !val4) 
+  {
+    bool x=true;
+    InALine(x); 
+  }
+  else if(val0 && !val4) 
+  {
+    bool y=true;
+    Turn(y);
+  }
+  else if(!val0 && val4) 
+  {
+    bool y=false;
+    Turn(y);
+  }
+  else if(val0 && val4) 
+  {
+    stop();
+    delay(200);
+    
+  }
+  
+  delay(100); 
+}
+
+ // Look right
+int lookRight() {
+  myservo.write(45);
+  delay(500);
+  int d = readDistance();
+  delay(100);
+  myservo.write(90);
+  return d;
+}
+
+// Look left
+int lookLeft() {
+  myservo.write(135);
+  delay(500);
+  int d = readDistance();
+  delay(100);
+  myservo.write(90);
+  return d;
+}
+
+// these are along with the kine following
+ void InALine(bool x) //to move in a line
+{
+  if(x==true)
+  {
+    // analogWrite(rme,60);
+    // analogWrite(lme,60);
+    digitalWrite(rmb,LOW);
+    digitalWrite(rmf,HIGH);
+    digitalWrite(lmb,LOW);
+    digitalWrite(lmf,HIGH);
+    //Serial.println("forward");
+    // delay(2000);  
+  }
+  else //move backward
+  {
+    // analogWrite(rme,50);
+    // analogWrite(lme,50);
+    digitalWrite(rmb,HIGH);
+    digitalWrite(rmf,LOW);
+    digitalWrite(lmb,HIGH);
+    digitalWrite(lmf,LOW);
+    //Serial.println("backward"); 
+    // delay(2000);     
+  }
+}
+
+void Turn(bool y) //to turn
+{
+    // analogWrite(rme,50);
+    // analogWrite(lme,50); 
+    if (y==true) //move right
+    {
+    digitalWrite(rmb,LOW);
+    digitalWrite(rmf,LOW);
+    digitalWrite(lmb,LOW);
+    digitalWrite(lmf,HIGH);
+    //Serial.println("right");  
+    // delay(2000);    
+    }
+    else //move left
+    {
+    digitalWrite(rmb,LOW);
+    digitalWrite(rmf,HIGH);
+    digitalWrite(lmb,LOW);
+    digitalWrite(lmf,LOW);
+    //Serial.println("left");  
+    // delay(2000);    
+    }
+    
+}
+void stop()
+{
+    // analogWrite(rme,0);
+    // analogWrite(lme,0);
+    digitalWrite(rmb,LOW);
+    digitalWrite(rmf,LOW);
+    digitalWrite(lmb,LOW);
+    digitalWrite(lmf,LOW);
+    //Serial.println("stop");    
+}
